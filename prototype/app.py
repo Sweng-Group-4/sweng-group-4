8a8ecb4d4adce24e0753181939a4d670d0e1c951
Alex McHugh <alexbmchugh@gmail.com>
2024-02-10 14:16:03 +0000

    initial prototype commit

---
+++ prototype/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from search import search_db
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('search.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    if request.method == 'POST':
+        # Get the search term from the form
+        search_term = request.form['search_term']
+        
+        # Search the database
+        results = search_db(search_term)
+        print(results)
+
+        return render_template('search.html', image_filenames=results)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
---
+++ prototype/search.py
@@ -0,0 +1,59 @@
+from sentence_transformers import SentenceTransformer, util
+from PIL import Image
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams, PointStruct
+import glob
+from tqdm import tqdm
+
+def get_filenames():
+    files = glob.glob('static/animals/**/*.jpg',  
+                   recursive = True) 
+    return files
+
+def retrieve_matches(hits):
+    filenames = []
+    for hit in hits:
+        filenames.append(hit.payload["filepath"])
+    return filenames
+
+def create_embeddings():
+
+    client = QdrantClient(path="vector_db")
+    model = SentenceTransformer('clip-ViT-B-32')
+
+    client.recreate_collection(
+        collection_name="animal_images",
+        vectors_config=VectorParams(size=512, distance=Distance.COSINE),
+    )
+
+    vectors = []
+
+    for f in tqdm(get_filenames()):
+        img_emb = model.encode(Image.open(f))
+        vectors.append([f, img_emb])
+
+    client.upsert(
+        collection_name="animal_images",
+        points=[
+            PointStruct(
+                id=idx,
+                vector=vector[1].tolist(),
+                payload={"filepath": vector[0]},
+            )
+            for idx, vector in enumerate(vectors)
+        ]
+    )
+
+def search_db(query):
+    client = QdrantClient(path="vector_db")
+    model = SentenceTransformer('clip-ViT-B-32')
+
+    text_emb = model.encode(query)
+
+    hits = client.search(
+        collection_name="animal_images",
+        query_vector=text_emb,
+        limit=20
+    )
+
+    return retrieve_matches(hits)
