091fcd80f281a186bcebcb456e77fd748118bc3c
mchugha2 <alexbmchugh@gmail.com>
2024-02-13 14:08:46 +0000

    change addr again

--- prototype/app.py
+++ prototype/app.py
@@ -20,4 +20,4 @@ def search():
         return render_template('search.html', image_filenames=results)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=80)
