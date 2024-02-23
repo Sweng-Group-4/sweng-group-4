a29379efaf392c71f2e21ea6e7ccc2377319ebd0
kburke999 <kburke9@tcd.ie>
2024-02-23 12:12:27 +0000

    Added background images to web pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import './About.css'; 
 
 const About = () => {
-    const backgroundImageUrl = 'https://images.unsplash.com/photo-1547103106-9a0e718bb2d2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
+    //const backgroundImageUrl = 'https://images.unsplash.com/photo-1547103106-9a0e718bb2d2?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    //const backgroundImageUrl = 'https://plus.unsplash.com/premium_photo-1684949051297-341c40d039c6?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    const backgroundImageUrl = 'https://images.unsplash.com/photo-1577139996275-6681fdc2165a?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    
     return (
         <div className="about-container" style={{backgroundImage: `url(${backgroundImageUrl})`}}>
-            <div className="content">
-                <h1 className="title">About Us</h1>
-                <p className="description">
-                    Lets meet the team:
-                </p>
-                {/* Add more content as needed */}
+            <div className="orange-box">
+                <div className="content">
+                    <h1 className="title">About Us</h1>
+                    <p className="description">
+                        Lets meet the team:
+                    </p>
+                    {/* Add more content as needed */}
+                </div>
             </div>
         </div>
     );
 };
 
 export default About;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Home.css';
 
 const About = () => {
-    const backgroundImageUrl = 'https://images.unsplash.com/photo-1613374933062-7aa9f54036ce?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
+    //const backgroundImageUrl = 'https://images.unsplash.com/photo-1613374933062-7aa9f54036ce?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    const backgroundImageUrl ='https://images.unsplash.com/photo-1531142590839-b096c1a9a7f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     return (
         <div className="about-container" style={{backgroundImage: `url(${backgroundImageUrl})`}}>
             <div className="content-box">
-                <h1 className="title"></h1>
+                <h1 className="title">Welcome to Shutter!</h1> {/* Add a title */}
                 <p className="description">
-                    Hi we are Shutter!
+                    Hi, we are Shutter!
                 </p>
             </div>
         </div>
