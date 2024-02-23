a57c4b750cb3e675a04cb1c3e03b427f58eb8d7c
kburke999 <kburke9@tcd.ie>
2024-02-23 12:12:27 +0000

    Added background images to web pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 const Contact = () => {
+    const backgroundStyle = {
+        backgroundImage: `url('https://images.unsplash.com/photo-1534689358699-037f71a746e5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: '#fff',
+        textAlign: 'center',
+        padding: '20px'
+    };
+
     return (
-        <div>
-            <h1>Contact page</h1>
+        <div style={backgroundStyle}>
+            <div>
+                <h1>Contact Us</h1>
+                <p>We'd love to hear from you. Send us a message!</p>
+                {/* contact information */}
+            </div>
         </div>
     );
 };
