022ccfc9a6b2e3eaf2dc59f2eb128c56017fb97a
seandrew11 <seandrew@tcd.ie>
2024-02-29 14:55:55 +0000

    Update fileUpload.test.jsx

--- src/pages/fileUpload.test.jsx
+++ src/pages/fileUpload.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from './fileUpload'; 
+import App from './FileUpload'; 
 // Test 1: Checks if Image Analyzer header Renders
 describe('App Component', () => {
   test('renders Image Analyzer header', () => {
